4cf9b1867648baea4bf31b7d54398d29ce6eb6d3
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-04-26 10:48:02 +0600

    disabled CORS and send categories data to the server

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 5000;
 
+const categories = require('./data/categories.json')
+
+app.use(cors())
+
 app.get('/', (req, res) => {
     res.send('Hello World!!')
+})
+
+app.get('/categories', (req, res) => {
+    res.send(categories)
 })
 
 app.listen(port, () => {
